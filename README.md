94813420ff3f9338fe8f4ad05a3e6d10d87c05d6
Sarah Allen <sarahallen@google.com>
2017-10-11 05:00:44 -0700

    write to firestore

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+
+```
+curl 'https://us-central1-faker-function.cloudfunctions.net/addDocument?name=foo&value=Hello!'
+```
+
+reports new ID of document added to messages collection:
+```
+{"result":"Message with ID: g98eVps6DEwrkFjUMdjX added."}
+```
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,29 @@
 import * as functions from 'firebase-functions'
 
 // if you need to use the Firebase Admin SDK, uncomment the following:
-// import * as admin from 'firebase-admin'
-
+import * as admin from 'firebase-admin'
+admin.initializeApp(functions.config().firebase);
 
 // Create and Deploy Cloud Function with TypeScript using script that is
 // defined in functions/package.json:
 //    cd functions
 //    npm run deploy
 
-export let helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!\n\n");
+export let helloWorld = functions.https.onRequest((req, res) => {
+ res.send("Hello from Firebase!\n\n");
 });
+
+
+export let addDocument = functions.https.onRequest((req, res) => {
+  console.log('req.query', req.query);
+  const name = req.query.name;
+  const value = req.query.value;
+  let obj = {};
+  obj[name] = value;
+
+  admin.firestore().collection('messages').add(obj).then(writeResult => {
+    // Send back a message that we've succesfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+  });
+});
+
